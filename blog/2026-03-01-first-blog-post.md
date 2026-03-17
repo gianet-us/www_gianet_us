f2b5cd6df1522632ccdd90377e0d66587d6d7fc0
Luciano Giacchetta <luciano@gianet.us>
2026-03-17 15:01:54 -0300

    fix: remove unused / demos

--- blog/2026-03-01-first-blog-post.md
+++ blog/2026-03-01-first-blog-post.md
@@ -1,8 +1,8 @@
 ---
 slug: first-blog-post
 title: First Blog Post
-authors: [slorber, yangshun]
-tags: [hola, docusaurus]
+authors: [giacchetta]
+tags: [hello]
 ---
 
 Lorem ipsum dolor sit amet...
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -87,7 +87,7 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Docs',
         },
         {to: '/blog', label: 'Blog', position: 'left'}
       ],
@@ -99,7 +99,7 @@ const config: Config = {
           title: 'Docs',
           items: [
             {
-              label: 'Tutorial',
+              label: 'Docs',
               to: '/docs/intro',
             },
           ],
